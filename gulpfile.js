8e39e8d07749e5e44c74177020b0cc8b1e54f036
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-10-18 04:05:08 -0400

    Add gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat')
+    concat = require('gulp-concat'),
+    compass = require('gulp-compass');
 
 var jsSources = [
   'components/scripts/get-images.js',
   'components/scripts/image-modal.js'
 ]
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('log', function() {
   gutil.log("workflows are awesome!")
@@ -17,6 +19,18 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(gulp.dest('builds/development/js'))
+    //.pipe(connect.reload())
+})
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      //image: 'build/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
     //.pipe(connect.reload())
 })
 
