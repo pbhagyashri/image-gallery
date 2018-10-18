8cf4fe5658eef75b251db77b1615d1a49f154b88
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-10-18 03:47:42 -0400

    Add gulp js task to combine all js files

---
+++ builds/development/js/script.js
@@ -0,0 +1,2 @@
+console.log("Fetch images from Flickr")
+console.log("Create image modal")
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 //all the variables and properties form following plugin are going to be assigned to corrosponding variable.
 
 var gulp = require('gulp'),
-    gutil = require('gulp-util')
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat')
+
+var jsSources = [
+  'components/scripts/get-images.js',
+  'components/scripts/image-modal.js'
+]
 
 gulp.task('log', function() {
   gutil.log("workflows are awesome!")
 })
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
+    //.pipe(connect.reload())
+})
+
+
+
