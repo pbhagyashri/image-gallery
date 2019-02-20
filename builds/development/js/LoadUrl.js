d5b36cf42cabd61fe0ebffd8961bdb5fb3faffa1
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2019-02-20 15:52:06 -0500

    Add index.html, script.js and LoadUrl.js files

---
+++ builds/development/js/LoadUrl.js
@@ -0,0 +1,151 @@
+
+//create a function to generate img tag
+function createImg(src, alt, title) {
+	var img = document.createElement('img');
+	img.src = src;
+	if ( alt != null ) img.alt = alt;
+	if ( title != null ) img.title = title;
+  
+	img.className = "grid-container__thumbnails"
+	return img;
+  }
+  
+  //This function will remove existing images from gallery 
+  function removeChildElements() {
+    //grid-container holds images that are loaded in the dom as it's child nodes.
+    let thumbnailDiv = document.getElementsByClassName("grid-container")[0]
+  
+    //run a loop to remove child nodes
+    while (thumbnailDiv.hasChildNodes()) {
+      thumbnailDiv.removeChild(thumbnailDiv.firstChild);
+    }
+  }
+  
+  //handle User Input or search query
+  let inputTag = document.getElementsByClassName('seacrh-form__input')[0]
+  let inputBtn = document.getElementsByClassName('search-form__btn')[0]
+  
+  inputBtn.addEventListener("click", (e) => {
+    e.preventDefault()  
+    removeChildElements()
+    let searchInput = inputTag.value
+  
+    if (searchInput == "") {
+      searchInput = 'vintage'
+    }
+    getPhotosByPage(1, searchInput)
+  })
+  
+  let paginationBtns = document.getElementsByClassName("pagination-container__button")
+  
+  for(let i = 0; i < paginationBtns.length; i++) {
+	
+    paginationBtns.item(i).addEventListener('click', function() {
+      removeChildElements()
+      if (inputTag.value == "") {
+        inputTag.value = 'vintage'
+      }
+  
+      pageNumber = parseInt(this.dataset.id)
+      getPhotosByPage(pageNumber, inputTag.value)
+    })
+  }
+  
+  window.addEventListener("load", function() {
+	LoadUrl()
+  })
+  
+  // // wrap api call in a function to make it reusable. this function will take page number as an argument and make fetch call to the api based on the provided page number. 
+  function getPhotosByPage(pageNumber = 1, searchQuery = 'vintage') {
+  
+    const KEY = 'f1b7bf92f0d79937504418aeaaad3907aa12ce5f5c8b06982e291d68b875d5db'
+    let URL = `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${searchQuery}&client_id=${KEY}`
+  
+    //make a fetch call to Unsplash api. Provide pageNumber as a variable to fetch URL
+    fetch(`${URL}`)
+      .then( res => res.json())
+      .then(response => {
+  
+        //query the grid container div and save it in a variable.
+        let thumbnailDiv = document.getElementsByClassName("grid-container")[0]
+  
+        if(response.results.length === 0) {
+          errorMessage = document.createElement('p')
+          errorMessage.className = "error"
+  
+          errorMessage.append("Sorry no images found :( Please try different keyword!")
+          thumbnailDiv.appendChild(errorMessage)
+        }
+		
+        //iterate over the JSON data returned by api call 
+        response.results.forEach(photo => {
+		  
+          // create a thumbnail container div for each photo returned by api
+          let thumbnailContainer = document.createElement('div');
+          thumbnailContainer.className = "thumbnail-container"
+		  
+          //create img tag for each photo returned by api
+          let src = photo.urls.thumb
+          //let src = photo.preview_photos[0].urls.thumb
+          let title = photo.title
+          let alt = photo.description
+          let imgTag = createImg(src, alt, title)
+          //append the image tag to  thumbnail container div
+          thumbnailContainer.appendChild(imgTag)
+  
+          imgTag.addEventListener('click', function() {
+            createModel(photo, this)
+          })
+  
+          //append thumbnail container div to main grid-container
+          thumbnailDiv.appendChild(thumbnailContainer)
+        })
+      })
+      .catch(error => {
+	  
+        alert(error)
+      })
+  }
+  
+  function createModel (photo, thumbnail) {
+    document.getElementById("modal").style.display = 'block'
+    thumbnail.src = photo.urls.regular
+    thumbnail.className = 'grid-container__small'
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+        thumbnail.className = 'grid-container__thumbnails'
+    }
+  }
+  
+  
+  
+  
+  // console.log("Create image modal")
+  
+  let url = 'https://carouselexample.azurewebsites.net/api/values?Url=blog.atish.me';
+  const LoadUrl = function(urlString) {
+  
+	return fetch(url, {
+	  method: "GET",
+	  mode: "cors",
+	  origin: "*",
+	  headers: {
+		"Content-Type": "application/json",
+	  },
+	})
+	.then( res => res.json())
+	  .then(response => {
+	  return response
+	})
+	.catch(error => {
+	  console.log("error", error)
+	})
+		
+  }
+  
+	
--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,128 +1,104 @@
 
 //create a function to generate img tag
-function createImg(src, alt, title) {
+const createImg = function(src) {
   var img = document.createElement('img');
   img.src = src;
-  if ( alt != null ) img.alt = alt;
-  if ( title != null ) img.title = title;
+  //if ( alt != null ) img.alt = alt;
+  //if ( title != null ) img.title = title;
 
   img.className = "grid-container__thumbnails"
+  
   return img;
 }
 
-//This function will remove existing images from gallery 
-function removeChildElements() {
-  //grid-container holds images that are loaded in the dom as it's child nodes.
+const createGallery = function(response) {
+  //query the grid container div and save it in a variable.
   let thumbnailDiv = document.getElementsByClassName("grid-container")[0]
 
-  //run a loop to remove child nodes
-  while (thumbnailDiv.hasChildNodes()) {
-    thumbnailDiv.removeChild(thumbnailDiv.firstChild);
+  response.forEach(imageUrl => {
+    
+    // create a thumbnail container div for each photo returned by api
+    let thumbnailContainer = document.createElement('div');
+    thumbnailContainer.className = "thumbnail-container";
+  
+    //create img tag for each photo returned by api
+    let imgTag = createImg(imageUrl);
+    //append the image tag to  thumbnail container div
+    thumbnailContainer.appendChild(imgTag);
+
+    //append thumbnail container div to main grid-container
+    thumbnailDiv.appendChild(thumbnailContainer);
+    
+  });
+};
+
+//this function displays total word count
+const TotalWordCount = function(response) {
+  let totalCountContainer = document.getElementById("total-word-count");
+
+  totalCountContainer.innerHTML = response.totalWordCount
+  
+}
+
+//this function displays a list of words with their counts.
+const WordCount = function(responseObject) {
+  let wordCountList = document.getElementById("word-count-list");
+ 
+
+  for (let key in responseObject) {
+
+    let value = responseObject[key];
+    let listItem = document.createElement('li');
+
+    listItem.className = "word-count-list__item";
+
+    listItem.innerHTML = `${key} - ${value}`
+
+    wordCountList.appendChild(listItem)
+
   }
+
+};
+
+//let url = 'https://carouselexample.azurewebsites.net/api/values?Url=blog.atish.me';
+let url = 'https://carouselexample.azurewebsites.net/api/values?Url=https://www.latimes.com/';
+
+//This is the main function that makes fetch calls to provided API
+const LoadUrl = function(urlString) {
+
+  return fetch(urlString, {
+    method: "GET",
+    mode: "cors",
+    origin: "*",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then( res => res.json())
+	.then(response => {
+   
+    createGallery(response.imageUrls)
+    TotalWordCount(response)
+    WordCount(response.wordCount)
+    
+  })
+  .catch(error => {
+    console.log("error", error)
+  })
+      
 }
 
 //handle User Input or search query
 let inputTag = document.getElementsByClassName('seacrh-form__input')[0]
 let inputBtn = document.getElementsByClassName('search-form__btn')[0]
-
+ 
 inputBtn.addEventListener("click", (e) => {
   e.preventDefault()  
-  removeChildElements()
+   //removeChildElements()
   let searchInput = inputTag.value
-
-  if (searchInput == "") {
-    searchInput = 'vintage'
-  }
-  getPhotosByPage(1, searchInput)
+ 
+  // if (searchInput == "") {
+  //   searchInput = 'vintage'
+  // }
+  LoadUrl(searchInput)
 })
-
-let paginationBtns = document.getElementsByClassName("pagination-container__button")
-
-for(let i = 0; i < paginationBtns.length; i++) {
-  
-  paginationBtns.item(i).addEventListener('click', function() {
-    removeChildElements()
-    if (inputTag.value == "") {
-      inputTag.value = 'vintage'
-    }
-
-    pageNumber = parseInt(this.dataset.id)
-    getPhotosByPage(pageNumber, inputTag.value)
-  })
-}
-
-window.addEventListener("load", function() {
-  getPhotosByPage()
-})
-
-// // wrap api call in a function to make it reusable. this function will take page number as an argument and make fetch call to the api based on the provided page number. 
-function getPhotosByPage(pageNumber = 1, searchQuery = 'vintage') {
-
-  const KEY = 'f1b7bf92f0d79937504418aeaaad3907aa12ce5f5c8b06982e291d68b875d5db'
-  let URL = `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${searchQuery}&client_id=${KEY}`
-
-  //make a fetch call to Unsplash api. Provide pageNumber as a variable to fetch URL
-  fetch(`${URL}`)
-    .then( res => res.json())
-    .then(response => {
-
-      //query the grid container div and save it in a variable.
-      let thumbnailDiv = document.getElementsByClassName("grid-container")[0]
-
-      if(response.results.length === 0) {
-        errorMessage = document.createElement('p')
-        errorMessage.className = "error"
-
-        errorMessage.append("Sorry no images found :( Please try different keyword!")
-        thumbnailDiv.appendChild(errorMessage)
-      }
-      
-      //iterate over the JSON data returned by api call 
-      response.results.forEach(photo => {
-        
-        // create a thumbnail container div for each photo returned by api
-        let thumbnailContainer = document.createElement('div');
-        thumbnailContainer.className = "thumbnail-container"
-        
-        //create img tag for each photo returned by api
-        let src = photo.urls.thumb
-        //let src = photo.preview_photos[0].urls.thumb
-        let title = photo.title
-        let alt = photo.description
-        let imgTag = createImg(src, alt, title)
-        //append the image tag to  thumbnail container div
-        thumbnailContainer.appendChild(imgTag)
-
-        imgTag.addEventListener('click', function() {
-          createModel(photo, this)
-        })
-
-        //append thumbnail container div to main grid-container
-        thumbnailDiv.appendChild(thumbnailContainer)
-      })
-    })
-    .catch(error => {
-    
-      alert(error)
-    })
-}
-
-function createModel (photo, thumbnail) {
-  document.getElementById("modal").style.display = 'block'
-  thumbnail.src = photo.urls.regular
-  thumbnail.className = 'grid-container__small'
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() { 
-      modal.style.display = "none";
-      thumbnail.className = 'grid-container__thumbnails'
-  }
-}
-
-
-
-
-console.log("Create image modal")
